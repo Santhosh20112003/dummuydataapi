a6835efda4782c90e04cf88295913dd466c95481
Santhosh <shanmugamsanthosh22@gmail.com>
2023-09-27 00:05:54 +0530

    First

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// Middleware
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse request body as JSON
 
-app.get("/ecommerce",(req,res)=>{
-	res.sendFile(__dirname+'/ecommerce.json');
-})
+// MongoDB Connection
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("MongoDB database connection established successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the application if unable to connect to MongoDB
+  });
 
-app.get("/music",(req,res)=>{
-	res.status(200).sendFile(__dirname+'/music.json');
-})
+// Routes
+const ecommerceRouter = require("./routes/ecommerce");
+const musicRouter = require("./routes/music");
+app.use("/ecommerces", ecommerceRouter);
+app.use("/music", musicRouter);
 
-app.get("/ecommerce",(req,res)=>{
-	res.sendFile(__dirname+'/jokes.json');
-})
+// Default route
+app.get("/", (req, res) => {
+  res.send("Welcome to Santhosh Technologies Api Hub");
+});
 
-app.listen(3000,()=>{
-	console.log("From the Rest Api");
-})
-
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
