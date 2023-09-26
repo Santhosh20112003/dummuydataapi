012916683228f0779d8eaab82d216523897edca2
Santhosh <shanmugamsanthosh22@gmail.com>
2023-09-27 00:35:11 +0530

    First

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json()); // Parse request body as JSON
 
-// MongoDB Connection
+app.use(cors()); 
+app.use(express.json()); 
+
+
 mongoose
   .connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
@@ -22,21 +22,28 @@ mongoose
   })
   .catch((error) => {
     console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit the application if unable to connect to MongoDB
+    process.exit(1); 
   });
 
-// Routes
+
 const ecommerceRouter = require("./routes/ecommerce");
 const musicRouter = require("./routes/music");
 app.use("/ecommerces", ecommerceRouter);
 app.use("/music", musicRouter);
 
-// Default route
+
+
+
 app.get("/", (req, res) => {
   res.send("Welcome to Santhosh Technologies Api Hub");
 });
 
-// Start the server
+app.get("*", (req, res) => {
+  res.status(404).send("Content Not Found Here...☹️");
+});
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
